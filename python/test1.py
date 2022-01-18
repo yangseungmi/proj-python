01fb6bafd0a7ebef36d4d833764cabbcbf083b89
seungmi yang <0105627@gmail.com>
2022-01-19 03:00:04 +0900

    init and add file

---
+++ python/test1.py
@@ -0,0 +1,33 @@
+
+"""
+a= "{0:<10}".format("hi")
+b=f'{"bhc":^^10}'
+print(a+"33")
+print(b)
+c='  bh d  d '.strip()
+print(c,42)
+d=[1,2,3]
+d.append([4,5])
+print(d)
+
+str=['a','b','c']
+print(str.index('b'))
+print(str[2])
+str.insert(0,'first') 
+print(str)
+str.remove('a')
+print(str)
+
+dic ={'m':3,"n":"p"}
+print(dic.values())
+print(list(dic.keys()))
+
+isInvlole ='ms' in dic
+print(isInvlole)
+
+s1 = set([1,2,3])
+s1.remove(3)
+print(type(s1))
+"""
+pin="931103-2184219"
+print('19'+pin[:6])
